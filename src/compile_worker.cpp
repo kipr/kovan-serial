c8d44e5e6b610eb615320f12322f751f6e60e978
nzrock95 <nzrock95@gmail.com>
2013-06-26 01:30:11 -0500

    Use RootManager functions in a non-static manner

--- src/compile_worker.cpp
+++ src/compile_worker.cpp
@@ -130,7 +130,7 @@ Compiler::OutputList CompileWorker::compile()
 	if(terminals.isEmpty()) return ret;
 	
 	// Copy terminal files to the appropriate directories
-	ret << RootManager::install(terminals, USER_ROOT, m_name);
+	ret << RootManager(USER_ROOT).install(terminals, m_name);
 
 	return ret;
 }
